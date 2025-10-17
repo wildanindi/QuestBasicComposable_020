7ade6a429b677b737693f1ff00cd4d8e1930f16c
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-17 16:13:20 +0700

    Menambah compose Box Column Dan row, Menambah background berwarna Kuning/Yellow

--- app/src/main/java/com/example/pertemuan3/Tataletak.kt
+++ app/src/main/java/com/example/pertemuan3/Tataletak.kt
@@ -1,16 +1,23 @@
 package com.example.pertemuan3
 
+import android.graphics.Paint
+import android.text.Layout
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 
 
@@ -86,6 +93,20 @@ fun TataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen3Kolom2")
         }
     }
+}
 
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier){
+    val gambar = painterResource(id = R.drawable.asd)
+    Column {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height( height = 110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        ){
 
+        }
+    }
 }
