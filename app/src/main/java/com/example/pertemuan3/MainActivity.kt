70db2993aa85758983ed8f9cbb74fded0a3592fd
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-10-17 15:45:38 +0700

    Menambah compose Box (teks bertumpukan)

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TataletakColumn(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/pertemuan3/Tataletak.kt
+++ app/src/main/java/com/example/pertemuan3/Tataletak.kt
@@ -1,12 +1,15 @@
 package com.example.pertemuan3
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -29,5 +32,20 @@ fun TataletakRow(modifier: Modifier) {
         Text(text = "Komponen2")
         Text(text = "Komponen3")
         Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
     }
 }
